6d4d5d7c086b7eb1465a5781cddbf1b460863c14
sebghub <seb.so@hotmail.com>
2016-05-02 10:25:00 +0200

    TImestamp funktionalitet
    -Skriver och läser till txt fil
    -KVARSTÅR: timestamp vid blippning (ska röra runt lite i refereringarna
    för att få det att fungera)

--- UserAuthentication.java
+++ UserAuthentication.java
@@ -1,26 +1,33 @@
-﻿import java.util.HashMap;
+import java.util.HashMap;
 
 import com.sun.net.httpserver.BasicAuthenticator;
 
 public class UserAuthentication extends BasicAuthenticator{
 		private UserContainer container = new UserContainer("filer/userList.txt");
+		private TimestampLog log = new TimestampLog("filer/timestampLog.txt");
 		public UserAuthentication(String message) {
 		super(message);
 	}
 
 		public boolean checkCredentials(String user, String pass) {
 			container.test(); /*Kommentera denna rad när UserContainer inte ska testas*/
+			log.test(); /*Kommentera denna rad när TimestampLog inte ska testas*/
 			HashMap<String,String> list = container.getAcceptanceList();
 			System.out.println("USER: "+user+" "+"PASS: "+pass+" END ");
-
+			
 			System.out.println("Empty? :" + list.isEmpty()+
 			"\n"+"Size: "+list.size());
 			System.out.println("list.containsKey(user) :"+list.containsKey(user));
 			System.out.println("list.get(user) :"+list.get(user));
-
+			
 			if(list.containsKey(user.hashCode()+"") && list.get(user.hashCode()+"").equals(pass.hashCode()+"")){
+				
+				System.out.println("Inloggning lyckad: "+user);
+				log.addTimestamp(user,true);
 				return true;
 			}else{
+				System.out.println("Inloggning misslyckad: "+user);
+				log.addTimestamp(user,false);
 				return false;
 			}
 		}
