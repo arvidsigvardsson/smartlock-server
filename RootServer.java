932f723d75440528c45860faf22d3522d8c976a4
Arvid Sigvardsson <arvid.sigvardsson@gmail.com>
2016-05-07 22:22:17 +0200

    Gjort om ios push med timeout

--- RootServer.java
+++ RootServer.java
@@ -1,10 +1,10 @@
 public class RootServer {
 	private static boolean openStatus = false;
-	private static boolean iosPushDataAvailable = false;
+	// private static boolean iosPushDataAvailable = false;
 	private static DataContainer dataContainer = new DataContainer("filer/idlist.txt", "filer/idNameMap.txt");
 	private static PushNotifier pushNotifier = new PushNotifier("filer/apikey.txt", "filer/pushtokens.txt");
-
 	private static TimestampLog timestampLog = new TimestampLog("filer/timestampLog.txt");
+	private static LongPollingPushMessage iosPushMessage = new LongPollingPushMessage("Invalid message", 0);
 
 
 	public static TimestampLog getTimestampLog() {
@@ -27,12 +27,21 @@ public class RootServer {
 		openStatus = status;
 	}
 
-	public static boolean getIosPushDataAvailable() {
-		return iosPushDataAvailable;
+	// public static boolean getIosPushDataAvailable() {
+	// 	return iosPushDataAvailable;
+	// }
+
+	// public static void setIosPushDataAvailable(boolean state) {
+	// 	iosPushDataAvailable = state;
+	// }
+
+	public static LongPollingPushMessage getIosPushMessage() {
+		return iosPushMessage;
 	}
 
-	public static void setIosPushDataAvailable(boolean state) {
-		iosPushDataAvailable = state;
+	public static void setIosPushMessage(LongPollingPushMessage message) {
+		System.out.println("Message: " + message);
+		iosPushMessage = message;
 	}
 
 	public static void main(String[] args) {
